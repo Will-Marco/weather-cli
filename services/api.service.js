9144582511cf8ad8ed2fc8daeac8146295efa043
Will-Marco <mr.marco0101@gmail.com>
2023-12-02 15:05:10 +0500

    changed method to axios

--- services/api.service.js
+++ services/api.service.js
@@ -1,5 +1,4 @@
-import https from "https";
-import { URL } from "url";
+import axios from "axios";
 import { TOKEN_LIB, getKeyValue } from "./storage.service.js";
 
 const getWeather = async (city) => {
@@ -9,21 +8,18 @@ const getWeather = async (city) => {
     throw new Error("API dosn't exist, -t: [API_KEY] for saving token ");
   }
 
-  const url = new URL("https://api.openweathermap.org/data/2.5/weather");
-  url.searchParams.append("q", city);
-  url.searchParams.append("appid", token);
-  url.searchParams.append("lang", "en");
-  url.searchParams.append("units", "metric");
-
-  https.get(url, (response) => {
-    let res = "";
-    response.on("data", (chunk) => {
-      res += chunk;
-    });
-    response.on("end", () => {
-      console.log(res);
-    });
-  });
+  const { data } = await axios.get(
+    "https://api.openweathermap.org/data/2.5/weather",
+    {
+      params: {
+        q: city,
+        appid: token,
+        lang: "en",
+        units: "metric",
+      },
+    }
+  );
+  return data;
 };
 
 export { getWeather };
